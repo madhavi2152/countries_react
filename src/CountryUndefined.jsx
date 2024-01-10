a95c3b343308d602191fa90f1bac3ab57f384975
madhavi2152 <madhavisharma2152@gmail.com>
2024-01-10 14:03:15 +0530

    files committed

--- src/CountryUndefined.jsx
+++ src/CountryUndefined.jsx
@@ -12,17 +12,16 @@ function CountryUndefined() {
       <header style={{ display: "none" }}></header>
       <button
         style={{
-          border: "1px solid black",
+          border: "2px solid black",
           margin: "100px",
-          marginLeft: "200px",
+          marginLeft: "400px",
           width: "100px",
           height: "50px",
           padding: 0,
           textDecoration: "none",
           background: "none",
           cursor: "pointer",
-          border: mode ? "hsl(209, 23%, 22%)" : "hsl(0, 0%, 100%)",
-          color: mode ? "hsl(200, 15%, 8%)" : "hsl(0, 0%, 100%)",
+          backgroundColor: "white",
         }}
         onClick={handleGoBack}
       >
